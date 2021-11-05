4e2aad671fa93b556400d3d4ed01fa5c86c5dd1b
dasprhr <dasprhr@gmail.com>
2021-11-05 16:46:17 +0000

    added label again

--- main.tf
+++ main.tf
@@ -47,7 +47,9 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  label        = "devops"
+  label {
+    name = "devops"
+  }
 
   boot_disk {
     initialize_params {
